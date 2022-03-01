5dc0c48ab05123c6e59fcc10c7eb2771acdd4fba
Muhammad Fajrianto <55910561+Fajrainto29@users.noreply.github.com>
2022-03-01 19:34:25 +0700

    Update Tugas_Pointer2.cpp

--- Tugas_Pointer2.cpp
+++ Tugas_Pointer2.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main() {
-    cout << "Nama : Muhammad Fajrianto\nNPM : G1A019037\n"<<endl;
-    int a = 10;
-    int b = 15;
+    	cout << "Nama : Muhammad Fajrianto\nNPM : G1A019037\n"<<endl;
+    	int a = 10;
+    	int b = 15;
 	int c = 27;
 	int *p1, *p2, *p3;
-
 	p1 = &a;
 	p2 = &b;
 	*p1 = c;
@@ -17,7 +16,6 @@ int main() {
 	p3 = &b;
 	p3 = &c;
 	*p1 = *p3;
-
 	cout<<"Nilai variabel a : "<<a<<endl;
 	cout<<"Nilai variabel b : "<<b<<endl;
 	cout<<"Nilai variabel c : "<<c<<endl;
@@ -27,6 +25,5 @@ int main() {
 	cout<<"Nilai yang tersimpan pada alamat p1 : "<<*p1<<endl;
 	cout<<"Nilai yang tersimpan pada alamat p2 : "<<*p2<<endl;
 	cout<<"Nilai yang tersimpan pada alamat p3 : "<<*p3<<endl;
-	
 	return 0;
 }
