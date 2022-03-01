4ae922d4484040b5f823373343e6600d6894f702
Muhammad Fajrianto <55910561+Fajrainto29@users.noreply.github.com>
2022-03-01 19:32:25 +0700

    Update Tugas_Pointer1.cpp

--- Tugas_Pointer1.cpp
+++ Tugas_Pointer1.cpp
@@ -8,14 +8,12 @@ int main() {
 	int b = 15;
 	int c = 27;
 	int *p1, *p2, *p3;
-
 	p1 = &b;
 	p2 = p1;
 	p1 = &c;
 	a = *p1;
 	p3 = &b;
 	*p2 = 8;
-
 	cout<<"Nilai variabel a : "<<a<<endl;
 	cout<<"Nilai variabel b : "<<b<<endl;
 	cout<<"Nilai variabel c : "<<c<<endl;
@@ -25,6 +23,5 @@ int main() {
 	cout<<"Nilai yang tersimpan pada alamat p1 : "<<*p1<<endl;
 	cout<<"Nilai yang tersimpan pada alamat p2 : "<<*p2<<endl;
 	cout<<"Nilai yang tersimpan pada alamat p3 : "<<*p3<<endl;
-	
 	return 0;
 }
